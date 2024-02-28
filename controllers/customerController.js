11402adfe01ba5fd03cc84aef3f8ef7dbcb03baf
rohan <rohansanjeev6464@gmail.com>
2024-02-28 14:16:14 +0530

    Add customer controller and routes

---
+++ controllers/customerController.js
@@ -0,0 +1,14 @@
+const pool = require('../db');
+
+const getCustomers = (req, res) => {
+  // get data from db
+};
+
+const addCustomer = (req, res) => {
+  // insert customer 
+};
+
+module.exports = {
+  getCustomers,
+  addCustomer
+}
--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+const routes = require('./routes');
+app.use('/api', routes);
 
 app.listen(5000, () => {
   console.log('Server listening on port 5000');
